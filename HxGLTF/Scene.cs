23d306fbe405b329d832b028f1614ff0aea7fb47
Oktay Bekar <30942231+SameplayerDE@users.noreply.github.com>
2024-06-13 02:13:11 +0200

    Scene

--- HxGLTF/Scene.cs
+++ HxGLTF/Scene.cs
@@ -2,7 +2,12 @@
 {
     public class Scene
     {
-        public string Name;
-        public Node[] Nodes;
+        public string? Name;
+        public Node[]? Nodes;
+
+        public int[]? NodesIndices;
+
+        public bool HasName => Name != null;
+        public bool HasNodes => Nodes != null;
     }
 }
