e2409253500e2d4b281bc4a41303d9987ca82068
SameplayerDE <asameplayer@gmail.com>
2025-02-17 20:06:46 +0100

    bag textures

--- Pattemon/Scenes/Inventory/InventoryScene.cs
+++ Pattemon/Scenes/Inventory/InventoryScene.cs
@@ -1,7 +1,9 @@
-﻿using InputLib;
+﻿using System;
+using InputLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using PatteLib;
 using Pattemon.Engine;
 using Pattemon.Graphics;
 
@@ -16,6 +18,14 @@ public class InventoryScene : SceneA
     private const int _stateWaitFadeOut = 50;
     
     private int _state = 0;
+
+    private const int _categoryLaneX = 7;
+    private const int _categoryLaneY = 91;
+    private const int _categoryLaneW = 10;
+    private const int _categoryLaneH = 10;
+    private const int _categoryLaneSpace = 1;
+
+    private int _categoryLaneIndex = 0;
     
     public InventoryScene(Game game, object args = null, string contentDirectory = "Content") : base(game, args, contentDirectory)
     {
@@ -25,6 +35,8 @@ public class InventoryScene : SceneA
     {
         GraphicsCore.LoadTexture("inventory", @"Assets/bag_overlay.png");
         GraphicsCore.LoadTexture("inventory_bag", @"Assets/bag_sheet.png");
+        GraphicsCore.LoadTexture("inventory_category_lane_sheet", @"Assets/bag_icon.png");
+        GraphicsCore.LoadTexture("inventory_category_lane_cursor", @"Assets/bag_icon_cursor.png");
         _state = _stateFadeIn;
         return true;
     }
@@ -33,6 +45,8 @@ public class InventoryScene : SceneA
     {
         GraphicsCore.FreeTexture("inventory");
         GraphicsCore.FreeTexture("inventory_bag");
+        GraphicsCore.FreeTexture("inventory_category_lane_sheet");
+        GraphicsCore.FreeTexture("inventory_category_lane_cursor");
         return true;
     }
 
@@ -60,6 +74,16 @@ public class InventoryScene : SceneA
                 {
                     _state = _stateFadeOut;
                 }
+
+                if (KeyboardHandler.IsKeyDownOnce(Keys.Left))
+                {
+                    _categoryLaneIndex--;
+                }
+                if (KeyboardHandler.IsKeyDownOnce(Keys.Right))
+                {
+                    _categoryLaneIndex++;
+                }
+                _categoryLaneIndex = Utils.Wrap(_categoryLaneIndex, 0, 7);
                 break;
             }
             case _stateFadeOut:
@@ -90,11 +114,14 @@ public class InventoryScene : SceneA
         RenderCore.SetTopScreen();
         spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
         // render text
-        
-        // render icons
-        
         spriteBatch.Draw(GraphicsCore.GetTexture("inventory"), Vector2.Zero, Color.White);
-        spriteBatch.Draw(GraphicsCore.GetTexture("inventory_bag"), new Vector2(16, 16), Color.White);
+        // render icons
+        for (int iconIndex = 0; iconIndex < 8; iconIndex++)
+        {
+            spriteBatch.Draw(GraphicsCore.GetTexture("inventory_category_lane_sheet"), new Vector2(_categoryLaneX + ((_categoryLaneSpace + _categoryLaneW) * iconIndex), _categoryLaneY), new Rectangle(iconIndex == _categoryLaneIndex ? 10 : 0, iconIndex * 10, 10, 10), Color.White);
+        }
+        spriteBatch.Draw(GraphicsCore.GetTexture("inventory_category_lane_cursor"), new Vector2(_categoryLaneX + ((_categoryLaneSpace + _categoryLaneW) * _categoryLaneIndex) - 3, _categoryLaneY - 3), Color.White);
+        spriteBatch.Draw(GraphicsCore.GetTexture("inventory_bag"), new Vector2(16, 16), new Rectangle(0, 64 * _categoryLaneIndex, 64, 64), Color.White);
         // render content
         spriteBatch.End();
     }
