0ea5b9297c166038d966bdc4a06c9da748dcfee1
Oktay <oktay-georg.trippel@telekom.de>
2025-03-04 15:25:30 +0100

    calculator

---
+++ Pattemon/PoketchApps/Calculator/CalculationOperator.cs
@@ -0,0 +1,11 @@
+namespace Pattemon.PoketchApps.Calculator;
+
+public enum CalculationOperator
+{
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Result, // prints the result
+    Clear // clears everything
+}
--- Pattemon/PoketchApps/Calculator/CalculatorPoketchApp.cs
+++ Pattemon/PoketchApps/Calculator/CalculatorPoketchApp.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Pattemon.PoketchApps.Calculator;
 
 public class CalculatorPoketchApp(Game game, object args = null, string contentDirectory = "Content") : PoketchApp(game, args, contentDirectory)
 {
+    private CalculationOperator _operator;
+    private int _inMemory;
+    private int _onDisplay;
+    
     public override bool Init()
     {
         throw new System.NotImplementedException();
